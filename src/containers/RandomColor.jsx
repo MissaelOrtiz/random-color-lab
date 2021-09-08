c8988151a30684a3fc384e37f21cc3ab0ec355cd
Missael Ortiz <ortiz.missael80@gmail.com>
2021-09-08 11:05:57 -0700

    added baby!

--- src/containers/RandomColor.jsx
+++ src/containers/RandomColor.jsx
@@ -15,14 +15,13 @@ const colors = ['red',
 
 export default class RandomColor extends Component {
     state = {
-      color: 'beige',
-      image: ''
+      color: 'beige'
     }
 
     doRandomColorChange = () => {
       const randomColorIndex = Math.floor(Math.random() * colors.length);
       if(colors[randomColorIndex] === this.state.color) {
-        // SOMETHING GOES HERE
+        this.setState({ color: 'url(https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg)' });
       } else { this.setState({ color: colors[randomColorIndex] }); }
     }
 
